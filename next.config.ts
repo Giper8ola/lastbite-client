676febfef91badc4a1f0bcfedfd0bb6f2c6a1622
Giper8ola <matwey.peregudow2100@gmail.com>
2025-04-27 04:30:24 +0300

    update next cfg

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
+import { PHASE_DEVELOPMENT_SERVER } from 'next/constants';
+
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-	/* config options here */
+const nextConfig = (phase: never): NextConfig => {
+	const isDev = phase === PHASE_DEVELOPMENT_SERVER;
+	const basePath = '/lastbite-client';
+
+	return {
+		output: 'export',
+		basePath: isDev ? undefined : basePath,
+		assetPrefix: isDev ? undefined : basePath + '/',
+		images: {
+			unoptimized: true
+		},
+		publicRuntimeConfig: {
+			basePath: isDev ? undefined : basePath
+		}
+	};
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+import getConfig from 'next/config';
 import Image from 'next/image';
+
+const { publicRuntimeConfig } = getConfig();
+const basePath = publicRuntimeConfig.basePath ?? '';
 
 export default function Home() {
 	return (
 		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
 			<main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-				<Image className="dark:invert" src="/next.svg" alt="Next.js logo" width={180} height={38} priority />
+				<Image className="dark:invert" src={basePath + '/next.svg'} alt="Next.js logo" width={180} height={38} priority />
 				<ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
 					<li className="mb-2 tracking-[-.01em]">
 						Get started by editing{' '}
@@ -22,7 +26,7 @@ export default function Home() {
 						target="_blank"
 						rel="noopener noreferrer"
 					>
-						<Image className="dark:invert" src="/vercel.svg" alt="Vercel logomark" width={20} height={20} />
+						<Image className="dark:invert" src={basePath + '/vercel.svg'} alt="Vercel logomark" width={20} height={20} />
 						Deploy now
 					</a>
 					<a
@@ -42,7 +46,7 @@ export default function Home() {
 					target="_blank"
 					rel="noopener noreferrer"
 				>
-					<Image aria-hidden src="/file.svg" alt="File icon" width={16} height={16} />
+					<Image aria-hidden src={basePath + '/file.svg'} alt="File icon" width={16} height={16} />
 					Learn
 				</a>
 				<a
@@ -51,7 +55,7 @@ export default function Home() {
 					target="_blank"
 					rel="noopener noreferrer"
 				>
-					<Image aria-hidden src="/window.svg" alt="Window icon" width={16} height={16} />
+					<Image aria-hidden src={basePath + '/window.svg'} alt="Window icon" width={16} height={16} />
 					Examples
 				</a>
 			</footer>
