408cc7da9b115d65dc2ca49f400858478d31e66b
Giper8ola <matwey.peregudow2100@gmail.com>
2025-04-27 04:23:15 +0300

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,6 @@ export default function Home() {
 					</li>
 					<li className="tracking-[-.01em]">Save and see your changes instantly.</li>
 				</ol>
-
 				<div className="flex gap-4 items-center flex-col sm:flex-row">
 					<a
 						className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
@@ -54,15 +53,6 @@ export default function Home() {
 				>
 					<Image aria-hidden src="/window.svg" alt="Window icon" width={16} height={16} />
 					Examples
-				</a>
-				<a
-					className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-					href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<Image aria-hidden src="/globe.svg" alt="Globe icon" width={16} height={16} />
-					Go to nextjs.org →
 				</a>
 			</footer>
 		</div>
