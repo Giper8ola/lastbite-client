0e995a0d6baa962b23e170df634372b8fcdecef4
Giper8ola <matwey.peregudow2100@gmail.com>
2025-04-28 22:38:04 +0300

    refactor box-item

--- src/components/BoxItem.tsx
+++ src/components/BoxItem.tsx
@@ -29,66 +29,57 @@ interface BoxProps extends Props {
 export const BoxItem: React.FC<BoxProps> = ({ url, name, category, restaurant, address, score, price, toCart, className }) => {
 	const { isOpen, onOpen, onOpenChange } = useDisclosure();
 	return (
+		// eslint-disable-next-line jsx-a11y/click-events-have-key-events
 		<div
-			role={'button'}
+			role="button"
 			tabIndex={0}
 			onClick={onOpen}
-			onKeyDown={toCart ? (e) => e.key === 'Enter' && onOpen?.() : undefined}
 			className={
-				` bg-white w-[420px] h-[200px] rounded-[25px] shadow-md hover:cursor-pointer transition-transform duration-300 ease-in-out transform hover:scale-105 origin-center p-[15px] grid grid-rows-1 grid-cols-[140px_1fr_65%] gap-[3px] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#89E49D] ` +
-				className
+				`bg-white rounded-3xl shadow-md
+				   hover:cursor-pointer transition-transform
+				   duration-300 ease-in-out transform
+				   hover:scale-105 origin-center p-4 ` + className
 			}
 		>
-			<div className="row-start-1 col-start-1">
-				<Image className="rounded-[15px]" alt="Box logo" src={url} width={140} height={140} priority></Image>
-			</div>
-			<div className="col-start-3 row-start-1">
-				<div className="flex flex-col justify-between gap-2 col-span-2 px-2">
+			<div className="flex gap-x-1">
+				<Image className="rounded-2xl" alt="Box logo" src={url} width={130} height={130} priority></Image>
+				<div className="flex flex-col justify-between gap-2 col-span-2 pl-2 w-full">
 					<div className="flex flex-row justify-between">
 						<h1 className="font-bold text-xl">{name}</h1>
-						<div className="">
-							<Score number={score} />
-						</div>
+						<Score number={score} />
 					</div>
-					<div className="w-[250px] flex-1 flex flex-wrap gap-2">
+					<div className="flex-1 flex flex-wrap gap-2 w-[250px]">
 						{category.map((el, ind) => (
 							<div key={ind}>
-								<p key={ind} className="bg-[#D4D4D4] rounded-[15px] px-[6px] py-[4px] text-xs pl-1.5 text-gray-500 font-thin">
-									{el}
-								</p>
+								<p className="bg-[#D4D4D4] rounded-[15px] px-[6px] py-1 text-xs pl-1.5 text-gray-500 font-thin">{el}</p>
 							</div>
 						))}
 					</div>
-					<div className="flex items-center justify-between">
-						<div>
-							<div className="flex gap-[3px]">
-								<ChefHat size={18} />
-								<p className="text-sm">{'Ресторан ' + restaurant}</p>
-							</div>
-							<div className="px-1 flex justify-between">
-								<p className="text-xs font-thin text-gray-500">{address}</p>
-							</div>
-						</div>
-					</div>
 				</div>
 			</div>
-
-			<BoxlistModal
-				isOpen={isOpen}
-				onOpenChange={onOpenChange}
-				url={url}
-				name={name}
-				category={category}
-				restaurant={restaurant}
-				address={address}
-				score={score}
-				price={price}
-			></BoxlistModal>
-			{toCart && (
-				<div className="flex col-span-3 justify-end pr-5">
-					<ToCartButton price={price} />
+			<div className="flex items-center justify-between mt-3">
+				<div>
+					<div className="flex gap-[3px]">
+						<ChefHat size={18} />
+						<p className="text-sm">{'Ресторан ' + restaurant}</p>
+					</div>
+					<div className="px-1 flex justify-between">
+						<p className="text-xs font-thin text-gray-500">{address}</p>
+					</div>
 				</div>
-			)}
+				<BoxlistModal
+					isOpen={isOpen}
+					onOpenChange={onOpenChange}
+					url={url}
+					name={name}
+					category={category}
+					restaurant={restaurant}
+					address={address}
+					score={score}
+					price={price}
+				></BoxlistModal>
+				{toCart && <ToCartButton price={price} />}
+			</div>
 		</div>
 	);
 };
