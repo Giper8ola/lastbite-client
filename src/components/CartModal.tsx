44cdb2ea0db923e9835cf8acdef928852a8fb1bd
Tsimba <igri_find@mail.ru>
2025-05-01 19:59:40 +0300

    Added order page

--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 import { Modal, ModalContent, ModalBody, Button, ScrollShadow } from '@heroui/react';
+import { ArrowRight } from 'lucide-react';
 
-import HeaderButton from '@/features/HeaderButton';
-import { COLORS } from '@/utils/consts';
+import Link from 'next/link';
 
 import { BoxItem } from './BoxItem';
 
@@ -102,27 +102,11 @@ export default function CartModal({
 									/>
 								))}
 							</ScrollShadow>
-							<div className="col-start-3 shadow-[-6px_0px_8px_0px_rgba(0,0,0,0.3)] pt-5 pb-8 px-3 grid grid-rows-2">
-								<p className="text-[24px] font-bold px-5 ">Доставить по адресу:</p>
-								<div className="text-[24px] font-bold rows-end-1 px-5 grid grid-rows-4 ">
-									<p className="row-start-1 leading-[80px]">Cпособ оплаты</p>
-									<HeaderButton color={COLORS.primary} className="w-[100%] h-[54px] rounded-full"></HeaderButton>
-									<HeaderButton color={COLORS.primary} className="w-[100%] h-[54px] rounded-full"></HeaderButton>
-									<HeaderButton color={COLORS.primary} className="w-[100%] h-[54px] rounded-full"></HeaderButton>
-								</div>
-								<div className="row-start-3 items-center flex flex-col">
-									<Button
-										onPress={onClose}
-										variant="faded"
-										className={`w-[90%] h-20 text-[24px] font-bold border-[#89E49D]`}
-										style={{
-											backgroundColor: COLORS.primary
-										}}
-									>
-										ОПЛАТИТЬ
-									</Button>
-								</div>
-							</div>
+							<Link href={basePath + '/order'} className="w-[90%]">
+								<Button variant="faded" className="w-full h-12 text-[20px] font-bold border-c-primary bg-c-primary mb-5">
+									<p>Оформить заказ</p> <ArrowRight className="ml-5 " />
+								</Button>
+							</Link>
 						</ModalBody>
 					</>
 				)}
