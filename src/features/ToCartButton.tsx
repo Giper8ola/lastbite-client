8474991e675cadb39ea14247421081db149e0e4b
AleXxi1337 <orelate06@gmail.com>
2025-05-03 22:02:52 +0300

    size change

--- src/features/ToCartButton.tsx
+++ src/features/ToCartButton.tsx
@@ -33,20 +33,20 @@ export const ToCartButton: React.FC<Props> = ({ onClick, price, count }) => {
 					}
 				}}
 			>
-				<p className="font-bold whitespace-nowrap text-[16px] mx-[10px]">{Count < 1 ? price : price * Count}₽</p>
+				<p className="font-bold whitespace-nowrap text-[14px] mx-[10px]">{Count < 1 ? price : price * Count}₽</p>
 				<AnimatePresence mode="wait">
 					{Count < 1 ? (
 						<motion.button className={`${styles.btn}`} onClick={() => setCount(Count + 1)}>
-							<ShoppingCart size={20} strokeWidth={3} />
+							<ShoppingCart size={14} strokeWidth={3} />
 						</motion.button>
 					) : (
 						<>
 							<motion.button className={`${styles.btn}`} onClick={() => setCount(Count - 1)}>
-								<Minus size={20} strokeWidth={3} />
+								<Minus size={14} strokeWidth={3} />
 							</motion.button>
-							<p className={`font-bold whitespace-nowrap text-[16px] mx-[10px]`}>{Count > 0 ? Count : ''}</p>
+							<p className={`font-bold whitespace-nowrap text-[14px] mx-[10px]`}>{Count > 0 ? Count : ''}</p>
 							<motion.button className={`${styles.btn}`} onClick={() => setCount(Count + 1)}>
-								<Plus size={20} strokeWidth={3} />
+								<Plus size={14} strokeWidth={3} />
 							</motion.button>
 						</>
 					)}
