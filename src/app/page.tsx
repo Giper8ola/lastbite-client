11a7f5702d3c650851bc71096b4da1f8e65decd6
Giper8ola <matwey.peregudow2100@gmail.com>
2025-05-03 15:14:45 +0300

    rollback

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ const Home = () => {
 	return (
 		<div className="py-5">
 			<Container>
-				<div className="px-5 mt-[100px]">
-					<BoxList list={BOXES_LIST} toCart={true} cols={3} rows={1} className="gap-6" />
+				<div className="px-5 mt-[100px] grid grid-cols-3 grid-rows-1 gap-6">
+					<BoxList list={BOXES_LIST} toCart={true} />
 				</div>
 			</Container>
 		</div>
--- src/components/BoxList.tsx
+++ src/components/BoxList.tsx
@@ -1,43 +1,19 @@
-import { CSSProperties } from 'react';
-
 import { BoxItem } from '@/components/BoxItem';
 import { IBox } from '@/types';
 
-export function BoxList({
-	list,
-	toCart,
-	cols,
-	rows,
-	className
-}: {
-	list: IBox[];
-	toCart?: boolean;
-	cols?: number;
-	rows?: number;
-	className?: string;
-}) {
-	const styles: CSSProperties = {
-		...(cols && rows && { display: 'grid' }),
-		...(cols && { gridTemplateColumns: `repeat(${cols}, minmax(0, 1fr))` }),
-		...(cols && { gridTemplateRows: `repeat(${rows}, minmax(0, 1fr))` })
-	};
-
-	return (
-		<div style={styles} className={className}>
-			{list.map((el, ind) => (
-				<BoxItem
-					key={ind}
-					imageUrl={el.imageUrl}
-					name={el.name}
-					categories={el.categories}
-					restaurant={el.restaurant}
-					address={el.address}
-					score={el.score}
-					price={el.price}
-					toCart={toCart ?? false}
-					className="my-3 w-full"
-				/>
-			))}
-		</div>
-	);
+export function BoxList({ list, toCart }: { list: IBox[]; toCart?: boolean }) {
+	return list.map((el, ind) => (
+		<BoxItem
+			key={ind}
+			imageUrl={el.imageUrl}
+			name={el.name}
+			categories={el.categories}
+			restaurant={el.restaurant}
+			address={el.address}
+			score={el.score}
+			price={el.price}
+			toCart={toCart ?? false}
+			className="my-3 w-full"
+		/>
+	));
 }
