02b340345c102b88418fa8f8062f4a280635802f
Tsimba <igri_find@mail.ru>
2025-05-12 16:40:19 +0300

    fix TotalPrice

--- src/components/TotalPrice.tsx
+++ src/components/TotalPrice.tsx
@@ -2,12 +2,13 @@ import { useCartStore } from '@/stores/CartStore';
 
 export default function TotalPrice() {
 	const cost = useCartStore((state) => state.cost);
-	const commission = Math.round(cost * 0.15 * 100) / 100;
-	const delivery = Math.round(cost * 0.3 * 100) / 100;
+	const commission = Math.round(cost * 0.15);
+	const delivery = Math.round(cost * 0.3);
+	const totalPrice = Math.round(cost + commission + delivery);
 	return (
 		<>
 			<p>ИТОГО:</p>
-			<b className="font-bold text-3xl">{cost + commission + delivery}₽</b>
+			<b className="font-bold text-3xl">{totalPrice}₽</b>
 			<div className="py-14">
 				<div className="flex justify-between">
 					<p>Стоимость товаров:</p>
