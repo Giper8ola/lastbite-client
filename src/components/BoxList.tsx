888c259cdc97dd994379c0ef48647aa054a7b1b3
Tsimba <igri_find@mail.ru>
2025-05-12 16:33:39 +0300

    add BoxList filter

--- src/components/BoxList.tsx
+++ src/components/BoxList.tsx
@@ -2,39 +2,35 @@ import { BigBoxItem } from '@/components/BigBoxItem';
 import { BoxItem } from '@/components/BoxItem';
 import { IBox } from '@/types';
 
-export function BoxList({ list, toCart, big, className }: { list: IBox[]; toCart?: boolean; big?: boolean; className?: string }) {
-	if (!className) {
-		className = 'my-3 w-full';
-	}
-	return list.map((el, ind) =>
-		big ? (
-			<BigBoxItem
-				key={ind}
-				id={el.id}
-				imageUrl={el.imageUrl}
-				name={el.name}
-				categories={el.categories}
-				restaurant={el.restaurant}
-				address={el.address}
-				score={el.score}
-				price={el.price}
-				toCart={toCart ?? false}
-				className={className}
-			/>
-		) : (
-			<BoxItem
-				key={ind}
-				id={el.id}
-				imageUrl={el.imageUrl}
-				name={el.name}
-				categories={el.categories}
-				restaurant={el.restaurant}
-				address={el.address}
-				score={el.score}
-				price={el.price}
-				toCart={toCart ?? false}
-				className={className}
-			/>
-		)
-	);
+export function BoxList({
+	list,
+	toCart = false,
+	big,
+	className = 'my-3 w-full',
+	indexes
+}: {
+	list: IBox[];
+	toCart?: boolean;
+	big?: boolean;
+	className?: string;
+	indexes?: number[];
+}) {
+	const filteredList = indexes ? list.filter((_, index) => indexes.includes(index)) : list;
+
+	return filteredList.map((el, index) => {
+		const baseProps = {
+			id: el.id,
+			imageUrl: el.imageUrl,
+			name: el.name,
+			categories: el.categories,
+			restaurant: el.restaurant,
+			address: el.address,
+			score: el.score,
+			price: el.price,
+			toCart,
+			className
+		};
+
+		return big ? <BigBoxItem key={index} {...baseProps} /> : <BoxItem key={index} {...baseProps} />;
+	});
 }
